aa6025db4d89d3a604317668033d02f93296daa9
BlackHeart.Goon <EliteGreyIT67@users.noreply.github.com>
2023-12-09 20:01:29 -0500

    Update bootstrap.sh

--- Scripts/Bootstrap/Ubuntu/bootstrap.sh
+++ Scripts/Bootstrap/Ubuntu/bootstrap.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
 #Bootstrap the system
-rm -rf $2
-mkdir $2
+rm -rf "$2"
+mkdir "$2"
 if [ "$1" = "i386" ] || [ "$1" = "amd64" ] ; then
-  debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates bionic $2 http://archive.ubuntu.com/ubuntu
+  debootstrap --no-check-gpg --arch="$1" --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates bionic "$2" http://archive.ubuntu.com/ubuntu
 else  
-  qemu-debootstrap --no-check-gpg --arch=$1 --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates bionic $2 http://ports.ubuntu.com/ubuntu-ports
+  qemu-debootstrap --no-check-gpg --arch="$1" --variant=minbase --include=systemd,libsystemd0,wget,ca-certificates bionic "$2" http://ports.ubuntu.com/ubuntu-ports
 fi
 
 #Reduce size
 DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true \
- LC_ALL=C LANGUAGE=C LANG=C chroot $3 apt-get clean
+ LC_ALL=C LANGUAGE=C LANG=C chroot "$3" apt-get clean
 
 #Setup DNS
-echo "127.0.0.1 localhost" > $2/etc/hosts
-echo "nameserver 8.8.8.8" > $2/etc/resolv.conf
-echo "nameserver 8.8.4.4" >> $3/etc/resolv.conf
+echo "127.0.0.1 localhost" > "$2"/etc/hosts
+echo "nameserver 8.8.8.8" > "$2"/etc/resolv.conf
+echo "nameserver 8.8.4.4" >> "$3"/etc/resolv.conf
 
 #sources.list setup
-rm $2/etc/apt/sources.list
+rm "$2"/etc/apt/sources.list
 if [ "$1" = "i386" ] || [ "$1" = "amd64" ] ; then
-  echo "deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse" >> $2/etc/apt/sources.list
-  echo "deb-src http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse" >> $2/etc/apt/sources.list
+  echo "deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse" >> "$2"/etc/apt/sources.list
+  echo "deb-src http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse" >> "$2"/etc/apt/sources.list
 else  
-  echo "deb http://ports.ubuntu.com/ubuntu-ports bionic main restricted universe multiverse" >> $2/etc/apt/sources.list
-  echo "deb-src http://ports.ubuntu.com/ubuntu-ports bionic main restricted universe multiverse" >> $2/etc/apt/sources.list
+  echo "deb http://ports.ubuntu.com/ubuntu-ports bionic main restricted universe multiverse" >> "$2"/etc/apt/sources.list
+  echo "deb-src http://ports.ubuntu.com/ubuntu-ports bionic main restricted universe multiverse" >> "$2"/etc/apt/sources.list
 fi
 
 #tar the rootfs
-cd $2
-rm -rf ../ubuntu-rootfs-$1.tar.gz
+cd "$2 || exit" || exit
+rm -rf ../ubuntu-rootfs-"$1".tar.gz
 rm -rf dev/*
-GZIP=-9 tar -cvzf ../ubuntu-rootfs-$1.tar.gz ./*
+GZIP=-9 tar -cvzf ../ubuntu-rootfs-"$1".tar.gz ./*
